5282d8737164498724752bb6b05424019ca4bc7a
arambhat <arambhat@asu.edu>
2022-05-08 10:45:50 -0700

    Face-recognition: Raspberrypi facerecognition Final code.
    This is face-recognition using Pi and AWS backend code with
    version 2 architecture. This architecture uses API gateway to
    trigger lambda.

--- pi/demo.py
+++ pi/demo.py
@@ -1,4 +1,5 @@
 import time
+from threading import Timer
 import requests
 import boto3
 from botocore.exceptions import NoCredentialsError
@@ -13,15 +14,15 @@ import os
 import errno
 
 VIDEO_DURATION_IN_MILLIS = 4000
-VIDEO_FORMAT = '.mp4'
 IMAGE_FORMAT = '.png'
+FRAME_RATE = 20
 NUM_FRAMES = 600  # 5 minutes video == 600 frames @ 2 frames/sec.
 MAX_WORKERS = 1
-AWS_REGION = "us-east-1"
-s3InputBucket = "g45-frame-extraction-bucket"
-s3outputBucket = "g45-output-bucket"
-
-faceRecognitionApiUrl = "https://enwpn2jcw4.execute-api.us-east-1.amazonaws.com/faceRec/facerecogition"
+AWS_REGION = "<YOUR_AWS_REGION>"
+s3InputBucket = "<S3_BUCKET_NAME_FOR_FRAMES>"
+s3VideoBucket = "<S3_BUCKET_NAME_FOR_VIDEOS>"
+finalVideoName = "finalDemoVideo.mp4"
+faceRecognitionApiUrl = "<FACERECOGNITION_API_END_POINT_URL>"
 
 
 def createS3Bucket(bucketParams) -> object:
@@ -65,22 +66,29 @@ def uploadFrame(image_name):
         print("The file was not found!")
     except NoCredentialsError:
         print("Credentials not found!!")
+    
+    try:
+        os.remove(image_path)
+        print("Local file deleted!")
+    except OSError as e:
+        if e.errno != errno.ENOENT:
+            print("File does not exist, so ignoring deletion")
+        else:
+            print("Error in deleting " + image_name + " file")
     return result
 
 
 def sendGetRecognitionRequest(request):
     headers = {"Content-Type": "application/json"}
     start_timestamp = datetime.now()
-    print("Sending Get request to lambda")
     http_response = requests.get(url=faceRecognitionApiUrl, params=request, headers=headers)
     end_time = datetime.now()
     latency = end_time - start_timestamp
-    print("latency: " + str(latency.total_seconds()) + " seconds")
     print(http_response.text)
+    print("latency: " + str(round(latency.total_seconds(), 2)) + " seconds")
 
 
 def uploadFrameAndRequestRecognition(image_name):
-    print("Upload function started in threadid: " + str(threading.get_ident()))
     response = uploadFrame(image_name)
     if response:
         request = {
@@ -88,62 +96,48 @@ def uploadFrameAndRequestRecognition(image_name):
             "BucketName": "g45-frame-extraction-bucket"
         }
         sendGetRecognitionRequest(request)
-        try:
-            os.remove(os.path.join(os.getcwd(), image_name))
-            print("Local file deleted!")
-        except OSError as e:
-            if e.errno != errno.ENOENT:
-                print("File does not exist, so ignoring deletion")
-            else:
-                print("Error in deleting " + image_name + " file")
     return response
-
 
 def recordVideo():
     camera = PiCamera()
-    camera.resolution = (160, 160);
+    camera.resolution = (640, 640);
     camera.framerate = 30
-    capture_object = PiRGBArray(camera, size=(160, 160))
+    capture_object = PiRGBArray(camera, size=(640, 640))
     time.sleep(0.1)  # For the camera to get setup.
     # Initialising the opencv video writer object.
-    video_path = os.path.join(os.getcwd(), "finalDemoVideo.mp4")
-    w, h = (160, 160)
+    video_path = os.path.join(os.getcwd(), finalVideoName)
+    w, h = (640, 640)
     fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-    fps = 30
+    fps = FRAME_RATE
     writer = cv2.VideoWriter(video_path, fourcc, fps, (w, h))
     frame_count = 0
     bucket_params = {'name': s3InputBucket, 'region': AWS_REGION}
     # A one time check to make sure the input bucket exists in the AWS.
     # If it doesn't exist, the below API call will create one.
     getS3Bucket(bucket_params)
-    # Initialising Thread pool Executor
-    # executor = ThreadPoolExecutor(max_workers=MAX_WORKERS)
-    results = []
     timestamp = 0
-    for frame in camera.capture_continuous(capture_object, format='rgb', use_video_port=True):
+    for frame in camera.capture_continuous(capture_object, format='bgr', use_video_port=True):
         frame_count = frame_count + 1
         image = frame.array
         # The image is saved into the output video file.
         writer.write(image)
-        #timestamp = datetime.now()
-        #timestamp = timestamp.strftime("%d_%m_%Y_%H_%M_%S")
         timestamp = timestamp + 1
         image_name = str(timestamp) + IMAGE_FORMAT
-        cv2.imwrite(image_name, image)
+        resize_dimensions = (160, 160)
+        resize_image = cv2.resize(image, resize_dimensions, interpolation=cv2.INTER_AREA)
         capture_object.truncate(0)
-        # Upload one frame in every 15 frames. (0.5 seconds)
-        if frame_count % 15 == 0:
+        # Upload one frame in every (FRAME_REATE/2) frames. (0.5 seconds)
+        if frame_count % (FRAME_RATE/2) == 0:
+            cv2.imwrite(image_name, resize_image)
             worker_thread = threading.Thread(target=uploadFrameAndRequestRecognition, args=(image_name,))
             worker_thread.start()
-        if (frame_count / 15) == 15:  # exit condition
+        if (frame_count / (FRAME_RATE/2)) == NUM_FRAMES:  # exit condition
+            writer.release()
+            final_video_path = os.path.join(os.getcwd(), finalVideoName)
+            s3 = boto3.client("s3", region_name=AWS_REGION)
+            s3.upload_file(final_video_path, s3VideoBucket, finalVideoName)
             break
-
-
+    camera.close()
+    
 if __name__ == '__main__':
-    # image_name = '06_05_2022_18_30_00.png'
-    # request = {
-    #     "ImageName": image_name,
-    #     "BucketName": "g45-frame-extraction-bucket"
-    # }
-    # sendGetRecognitionRequest(request)
     recordVideo()
